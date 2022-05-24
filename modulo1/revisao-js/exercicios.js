3758a9afcbca4131bb9827ae17dcf96235340f49
kellynakaya <kelly_miya@outlook.com>
2022-05-24 00:01:33 -0300

    revisao-js

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,139 @@
+// ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    array = ["Azul", "Amarelo", "Vermelho"]
+    return array.length
+   
+} retornaTamanhoArray(array)
+
+console.log(retornaTamanhoArray(array))
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+    array = ["Laranja", "Branco", "Marrom"]
+    return array.reverse()
+  
+} retornaArrayInvertido(array)
+
+console.log (retornaArrayInvertido(array))
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+  array = ["Miguel", "Joaquim", "Bárbara", "Luciene"]
+  return array.sort()
+} retornaArrayOrdenado(array)
+
+console.log(retornaArrayOrdenado(array))
+
+// EXERCÍCIO 04
+
+//const array = [1, 2, 3, 4, 5, 6]
+
+function retornaNumerosPares(array) {
+  return array.filter(array => array % 2 === 0);
+} 
+retornaNumerosPares(array)
+
+console.log (retornaNumerosPares(array))
+
+// EXERCÍCIO 05
+
+/*const array = [1, 2, 3, 4, 5, 6]
+
+function retornaNumerosParesElevadosADois(array) {
+  array.filter(array => array % 2 === 0);
+   array.pow(2,4,6)
+   return
+}
+retornaNumerosParesElevadosADois(array) */
+
+// EXERCÍCIO 06
+
+const array = [1,2,3,4]
+
+function retornaMaiorNumero(array) {
+return maiorNumero = Math.max(...array);
+    
+} retornaMaiorNumero(array)
+
+console.log(retornaMaiorNumero(array))
+
+
+// EXERCÍCIO 07
+let doisNumeros = (2,4)
+
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+if()
+
+}
+
+// EXERCÍCIO 08
+const listaNumeros = [1,2,3,4,5,6]
+
+function retornaNPrimeirosPares(n) {
+    return listaNumeros.filter(listaNumeros => listaNumeros % 2 === 0);
+   
+} retornaNPrimeirosPares(3)
+
+console.log(retornaNPrimeirosPares(3));
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+
+function retornaChamadaDeFilme(filme) {
+        nome: "De repente 30",
+        ano: 2004,
+        diretor: "Gary Winick",
+        atores: "Jennifer Garner", "Mark Ruffalo", "Judy Greer"
+        
+} return
+
+retornaChamadaDeFilme(filme)
+
+console.log("Venha assistir ao filme " + (filme.nome) + ", de" (filme.ano) + ", dirigido por " + (filme.diretor) + " e estrelado por" + (filme.atores)
+
+// EXERCÍCIO 12
+
+/*function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
+*/
